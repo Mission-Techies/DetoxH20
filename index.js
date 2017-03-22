44ddfce7e3699b91f99eb5edd555d68654f0ae5e
92mm <marymedrano@missiontechiess-MacBook-Air-2.local>
2017-03-22 13:47:28 -0700

    got first page working

---
+++ index.js
@@ -0,0 +1,20 @@
+var detoxH20_data_url = 'https://api.airtable.com/v0/appme2bUqi2ONfNq6/Suppliers?api_key=key1I6vl8hY7lZc7U';
+var detoxH20HTML = '';
+var detoxH20Div = $('.detoxH20');
+var renderdetoxH20DivData = function (data) {
+  data.records.forEach(function(customer) {
+    if(customer.fields['Customer name']) {
+      detoxH20HTML  += '<h2>' + customer.fields['Customer name'] + '</h2>';
+      detoxH20HTML  += '<p>' + customer.fields['Orders'] + '</p>';
+      if(customer.fields['Notes']){
+        detoxH20HTML  += '<p>' + customer.fields['Notes'] + '</p>';
+      }
+      detoxH20HTML  += '<p>' + customer.fields['Paid?'] + '</p>';
+      detoxH20HTML += '<hr />';
+    }
+  });
+  detoxH20Div.html(detoxH20HTML);
+};
+
+//
+$.getJSON(detoxH20_data_url, renderdetoxH20DivData);
