728ca6923a1ac89a56fadd909e543cb4c157e34a
92mm <marymedrano@missiontechiess-MacBook-Air-2.local>
2017-03-24 13:57:37 -0700

    new

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ var renderdetoxH20DivData = function (data) {
 
       $.each(detoxH20Pictures, function(i, Pictures){
       detoxH20HTML += "</br>";
-        detoxH20HTML += `<img src="${Pictures.url}">`;
+        // detoxH20HTML += `<img src="${Pictures.url}">`;
+        detoxH20HTML += `<a href="detail.html?drinkID=${customer.id}"><img src="${Pictures.url}"></a>`;
       });
 
       detoxH20HTML  += '<p>' + customer.fields['Description'] + '</p>';
@@ -26,3 +27,32 @@ var renderdetoxH20DivData = function (data) {
 
 //
 $.getJSON(detoxH20_data_url, renderdetoxH20DivData);
+
+
+// ---------------------------------------------------
+// var all_murals= 'https://api.airtable.com/v0/appC2gzawbLgSvOeE/Art?api_key=' + api_key;
+//
+// // All details
+// function renderRecords(data) {
+//     $(data.records).each(function(index, mural) {
+//       var mural_name = mural.fields['Name/Description'];
+//       var mural_where = mural.fields['Where'];
+//       var mural_pics = mural.fields['Pic'];
+//       var mural_info = '';
+//       if (mural_name) {
+//         mural_info +=`<div class="media">`;
+//           mural_info +=`<div class="media-left">`;
+//           if (mural_pics) {
+//             $.each(mural_pics, function(i, pic){
+//               mural_info +=`<a href="detail.html?muralID=${mural.id}"><img src="${pic.url}"></a>`;
+//             });
+//           }
+//           mural_info +=`</div>`;
+//         mural_info += `<div class="media-body">${mural_name}</div>`;
+//         mural_info +=`</div>`;
+//       }
+//       $('.murals').append(mural_info);
+//     });
+// }
+//
+// $.get(all_murals, renderRecords);
