485835637a8e5f9e54988e4bbc36fbc1b280f3f5
92mm <marymedrano@missiontechiess-MacBook-Air-2.local>
2017-03-29 14:58:13 -0700

    table

--- detail.js
+++ detail.js
@@ -24,17 +24,21 @@ var url_one_drink = function() {
 function renderOneDrink(drink) {
       var drink_name = drink.fields['Drinks'];
       var ingredients = drink.fields['Ingredients'];
-      // var drink_pics = drink.fields['Pic'];
+      var drink_pics = drink.fields['Picture'];
       var drink_info = '';
 
 
       if (drink_name) {
-        drink_info += '<div>';
-          drink_info += ingredients;
-        drink_info += '</div>';
-        // $.each(drink_name, function(i, pic){
-        //   drink_info += ingredients;
-        // });
+        drink_info +=`<div class="panel panel-default>`;
+            drink_info +=`<div class="panel-body">`;
+               if (drink_pics) {
+                   $.each(drink_pics, function(i, pic){
+                     drink_info +=`<center><a href="detail.html?drinkID=${drink.id}"><img src="${pic.url}"></a></center>`;
+                   });
+               }
+            drink_info +=`</div>`;
+          drink_info += '<div class="panel-footer">' + ingredients + '</div>';
+        drink_info +=`</div>`;
 
       }
       $('.drink-detail').append(drink_info);
@@ -42,3 +46,53 @@ function renderOneDrink(drink) {
 
 
 $.get(url_one_drink(), renderOneDrink);
+
+var submitURL = 'https://api.airtable.com/v0/appme2bUqi2ONfNq6/Menu/ api_key=key1I6vl8hY7lZc7U';
+var form = $('#my-form');
+form.on('submit', function(e){
+   e.preventDefault();
+  var drink =$(this).find('input[name=drink]').val();
+  var name = $(this).find('input[name=count-of-people]').val();
+   var address = $(this).find('input[name=address]').val();
+   var city = $(this).find('input[name=city]').val();
+   var organization = $(this).find('input[name=organization]').val();
+   var time = $(this).find('input[name=time]').val();
+   var phone Number = $(this).find('input[name=phone Number]').val();
+   var name = $(this).find('input[name=name]').val();
+   var email = $(this).find('input[name=email]').val();
+   var questions and concerns = $(this).find('input[name=questions and concerns]').val();
+   var name = $(this).find('input[name=name]').val();
+   var when = $(this).find('input[name=when]').val();
+
+   if (!name) {
+      $(this).find('input[name=name]').addClass("error");
+     return;
+   }
+   if (!address) {
+     alert('no address');
+     return;
+   }
+  if (!phone){
+    alert('phone number missing');
+   return;
+  }
+  }
+  if (!email){
+    alert('email needed');
+   return;
+  }
+
+
+   var data = {
+     'fields': {
+       'Name': name,
+       'Home Address': address,
+       'Gender': gender,
+       'memo': memo,
+    }
+   };
+  $.post(submitURL, data, function(data){
+     $('#submit-message').text('Submitted!!!!');
+     console.log('success',data)
+  });
+});
